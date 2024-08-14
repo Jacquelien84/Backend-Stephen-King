d1622ff0a72472214028ea09d3c63c5cba99f232
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-08-14 11:28:57 +0200

    backend project eindopdracht

---
+++ src/main/java/nl/oudhoff/backendstephenking/BackendStephenKingApplication.java
@@ -0,0 +1,13 @@
+package nl.oudhoff.backendstephenking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStephenKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStephenKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/oudhoff/backendstephenking/BackendStephenKingApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.oudhoff.backendstephenking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStephenKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
