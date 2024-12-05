ca036751481bc78140f51dc3544ee1d3f4e86091
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-12-05 15:33:09 +0100

    IntegrationTest gemaakt

---
+++ README.md
@@ -0,0 +1,6 @@
+# Eindopdracht Novi Hogeschool
+
+![img_1.png](img_1.png)
+
+Voor de frontend kan je terecht op https://github.com/Jacquelien84/Frontend-Stephen-King
+
--- src/test/java/nl/oudhoff/backendstephenking/controller/BookControllerUnitTest.java
+++ src/test/java/nl/oudhoff/backendstephenking/controller/BookControllerUnitTest.java
@@ -1,49 +1,48 @@
 package nl.oudhoff.backendstephenking.controller;
 
-import nl.oudhoff.backendstephenking.model.Book;
-import nl.oudhoff.backendstephenking.repository.BookRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import nl.oudhoff.backendstephenking.dto.output.BookOutputDto;
+import nl.oudhoff.backendstephenking.service.BookService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class BookControllerIntegrationTest {
-
+@WebMvcTest(BookController.class)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = true)
+public class BookControllerUnitTest {
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
-    @Autowired
-    private BookRepository bookRepository;
+    @MockBean
+    BookService bookService;
 
-    @BeforeEach
-    void setUp() {
-        Book book1 = new Book();
-        book1.setTitle("Carrie");
-        book1.setAuthor("Stephen King");
-        book1.setOriginalTitle("Carrie");
-        book1.setReleased(1974L);
-        book1.setMovieAdaptation("Carrie - 1976, 2013");
-        book1.setDescription("Het verhaal gaat over Carrie, een zeventienjarig meisje met telekinetische gaven. Ze is een buitenbeentje op school en wordt vaak gepest en vernederd, ook door haar dominante moeder. Maar ze ontdekt haar krachten en krijgt deze steeds meer onder controle. Op het schoolbal gaan haar klasgenoten te ver en neemt ze wraak...");
-        bookRepository.save(book1);
-    }
-
+    //    Test 10
     @Test
-    @DisplayName("Should retrieve a book by ID")
-    @WithMockUser(username = "testuser", roles = "USER")
-    void shouldRetrieveBookById() throws Exception {
+    @WithMockUser(username="testuser", roles="USER")
+    void shouldRetrieveBook() throws Exception {
+        BookOutputDto bookDto = new BookOutputDto();
+        bookDto.setId(1L);
+        bookDto.setTitle("Carrie");
+        bookDto.setAuthor("Stephen King");
+        bookDto.setOriginalTitle("Carrie");
+        bookDto.setReleased(1974L);
+        bookDto.setMovieAdaptation("Carrie - 1976, 2013");
+        bookDto.setDescription("Het verhaal gaat over Carrie, een zeventienjarig meisje met telekinetische gaven. Ze is een buitenbeentje op school en wordt vaak gepest en vernederd, ook door haar dominante moeder. Maar ze ontdekt haar krachten en krijgt deze steeds meer onder controle. Op het schoolbal gaan haar klasgenoten te ver en neemt ze wraak...");
+
+        Mockito.when(bookService.getBookById(anyLong())).thenReturn(bookDto);
+
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/books/1"))
                 .andDo(MockMvcResultHandlers.print())
@@ -56,4 +55,3 @@ public class BookControllerIntegrationTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.description", is("Het verhaal gaat over Carrie, een zeventienjarig meisje met telekinetische gaven. Ze is een buitenbeentje op school en wordt vaak gepest en vernederd, ook door haar dominante moeder. Maar ze ontdekt haar krachten en krijgt deze steeds meer onder controle. Op het schoolbal gaan haar klasgenoten te ver en neemt ze wraak...")));
     }
 }
-
